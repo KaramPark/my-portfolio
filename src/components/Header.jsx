c17831d655d7bb88790e858f9f75c566d90f258c
KaramPark <com3892@gmail.com>
2025-08-12 14:18:51 +0900

    feature : about,skills,projects

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,13 @@ import { Button } from "@radix-ui/themes";
 import { FaGithubAlt, FaBlogger, FaMailBulk } from "react-icons/fa";
 
 const Header = ({ profile }) => {
-  useEffect(() => console.log(profile), []);
+   const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <header className=" min-h-screen flex flex-col justify-center items-center text-center px-4 bg-gradient-to-br from-blue-50 to-slate-50">
       <div className="max-w-4xl mx-auto">
@@ -43,6 +49,7 @@ const Header = ({ profile }) => {
               key={section}
               className="relative group animate-fade-up text-gray-600 transition-all duration-300 font-medium capitalize" 
               style={{ animationDelay: `${0.6 + index * 0.1}s` }}
+              onClick={() => scrollToSection(section)}
             >
             {section}
            <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-600 group-hover:w-full"></span></button>))}
